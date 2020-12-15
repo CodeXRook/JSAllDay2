1ac2209f2d444ccc7bfdc5a01a7b2f097383111b
CodeXrook <lacekitfly@gmail.com>
2020-12-15 17:52:32 -0500

    made changes to var

---
+++ variables.js
@@ -0,0 +1,23 @@
+// Variables are named containers.
+// We can place data in and then refer the data by the name in that container.
+// must begin with upper/lower case a - z, $, _
+// camelCase best practice
+
+var example = 'first example';
+
+/*
+FIX THE ERRORS
+var 1stPet = 'Bear';
+var BaseballTeam = 'Dodgers';
+var favoritedayoftheweek = 'Friday';
+var favorite color = 'green';
+var var = 'car';
+*/
+
+var firstPet = 'Bear';
+var baseBallTeam = 'Dodgers';
+var favoriteDayOfTheWeek = 'Friday';
+var favorite_color = 'green';
+var vehicle = 'car';
+
+
