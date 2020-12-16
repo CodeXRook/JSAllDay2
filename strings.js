75bc8b4c84cf0de1d6fadb20b4e94c99857f2c84
CodeXrook <lacekitfly@gmail.com>
2020-12-15 19:59:57 -0500

    Stringing along

--- strings.js
+++ strings.js
@@ -1,5 +1,28 @@
 // Strings are a datatype used to represent 
 // text and are wrapped in either a single or double quotes
+// We learned that strings are used to represent text
+// turn any primitive data type into a string 
+// by adding single or double quotes 
+
 
 var greeting = "it's good to meet you!";
 greeting.length;
+
+
+// challange 
+/* 
+var animal = 'horse";
+var answer = false;
+*/
+
+// valid strings
+var animal = 'horse';
+var answer = 'false';
+
+var greeting1 = 'Sam says, "hi."';
+greeting1;
+// "Sam says, "hi.""
+
+var greeting2 = "It's great to see you!";
+greeting2;
+// "It's great to see you!"
