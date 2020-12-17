b915bd9e88e72b4ff4fd85508bacd30c831be6bd
CodeXrook <lacekitfly@gmail.com>
2020-12-17 01:12:05 -0500

    Prr'n right along

---
+++ Cat_Years.js
@@ -0,0 +1,30 @@
+// Mini Project: Cat Years
+// Challenge: Convert Your Age to Cat Years
+
+// assign my age to the variable `myAge`
+var  myAge = 30;
+
+// assign value of `2` to variable `earlyYears`
+var earlyYears = 2;
+
+// multiply `earlyYears` by `25` to account for early growth rate
+earlyYears *= 25;
+
+// subtract `2` years from `myAge` and assign to `laterYears` to account for growth rate of a 
+// cat after the first two years
+
+var laterYears = myAge -2;
+
+laterYears *= 4;
+
+// add value of `earlyYears` and `laterYears` for the total age and assign to `myAgeInCatYears`
+var myAgeInCatYears = earlyYears + laterYears;
+
+
+// assign the value of my name to the variable `myName`
+var myName = 'Max';
+
+
+console.log('My name is ' + myName + '. I am ' + myAge 
++ ' years old in human years which is ' + myAgeInCatYears 
++ ' years old in cat years.');
