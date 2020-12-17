c7a4e5801f53013050015d4555eda8e8ebe89191
CodeXrook <lacekitfly@gmail.com>
2020-12-16 20:09:14 -0500

    mini kelvin t0 Fahrenheit temp

---
+++ kelvin-Fahrenheit.js
@@ -0,0 +1,17 @@
+/*
+In this mini-project we are going to create a mini application 
+in the console that converts the temperature
+from kelvin to fahrenheit.
+*/
+
+
+// temperature in celsius stored in variable `celsius`
+var kelvinTemp = 301;
+
+// temperature in celsius stored in variable `celsius`
+var celsiusTemp = kelvinTemp - 273.15;
+
+// convert celsius to fahrenheit stored in variable `fahrenheit
+var fahrenheitTemp = celsiusTemp * (9/5) + 32;
+
+// round the value of fahrenheit down and assign to `fahrenheit`
--- operators.js
+++ operators.js
@@ -11,3 +11,27 @@
    y++;
    ++y
 
+
+// The basic arithmetic operators are multiplication, division,
+// modulus, addition and subtraction.
+
+
+//COMPARISON 
+ // == Equal Value
+ // === Equal Value And Type
+
+ var x = 8;
+ var y = '8';
+
+ x == y; // true
+ x === y; // false
+
+ // plus operator is called the concatenation operator.
+var firstName = 'Rob';
+var middleName = 'Grant';
+var lastName = 'Merrill';
+
+var fullName = firstName + ' ' + middleName + ' ' + lastName;
+fullName;
+"Rob Grant Merrill"
+
