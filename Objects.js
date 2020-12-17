aed7a414b244ec9f70e2528f54e71542662ad19e
CodeXrook <lacekitfly@gmail.com>
2020-12-17 02:19:01 -0500

    put it in brackets

---
+++ Objects.js
@@ -0,0 +1,33 @@
+// an object is a value type consisting of key value pairs.
+// Javascript has two main value types primitives and objects.
+
+// What is a primitive? Strings, Numbers, Booleans, Null, Undefined.
+// and in ES6 Symbols
+
+// Variables become known as properties 
+// if a variable is part of an object it is called a property
+
+var car = {
+    wheels: 4,
+    color: 'white',
+    mpg: 34,
+    engine: 5,
+    gallons: 17
+};
+
+// DOT NOTATION
+
+var car = {};
+// object.property = value;
+
+car.wheels = 4;
+car.color = 'white';
+car.mpg = '34';
+car.engine = 5;
+car.gallons = 17;
+
+// BRACKET NOTATION
+
+var car = {};
+// object['property'] = value;
+
