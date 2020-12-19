819370245578b424879ff4907223f148cb8d1350
CodeXrook <lacekitfly@gmail.com>
2020-12-19 01:36:35 -0500

    Bear Human or Gun, rock paper scissors 2020 style

---
+++ bearHuman0rGun.js
@@ -0,0 +1,9 @@
+// Mini App: Bear, Human, Gun Game
+// Bear human gun is just like Rock paper scissors
+/*
+ Each player chooses either bear, human, or gun. 
+ The items are compared, and whichever player chooses 
+ the more powerful item wins.
+ */
+
+ 
