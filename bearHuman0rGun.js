814cafd4bd6fc8077ffbccb05f03bd6f0892ff6d
CodeXrook <lacekitfly@gmail.com>
2020-12-19 23:14:21 -0500

    create playgame funct

--- bearHuman0rGun.js
+++ bearHuman0rGun.js
@@ -6,22 +6,22 @@
  the more powerful item wins.
  */
 
- //  1 create function getUserChoice with a parameter of 'userInput'
- function getUserChoice(userInput) {
-    // 2 turn future `userInput` to lower case
+// create function getUserChoice with a parameter of `userInput`
+function getUserChoice(userInput) {
+    // turn future `userInput` to lower case
     userInput = userInput.toLowerCase();
-    // 3 if there is a match return `userInput`
+    // if there is a match return `userInput`
     if (userInput === 'bear' || userInput === 'human' || userInput === 'gun') {
         return userInput;
-      // 4 if no match prompt user for valid entry
+      // if no match prompt user for valid entry
     } else {
         return 'Please enter valid option';
     }
 }
 
-// 5 create a function that will provide a random computer choice for each round
-function getComputerChoice(){
-    var random = Math.floor(Math.random() * 3);
+// create a function that will provide a random computer choice for each round
+function getComputerChoice() {
+    var randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         return 'bear';
     } else if (randomNumber === 1) {
@@ -30,32 +30,39 @@ function getComputerChoice(){
         return 'gun';
     }
 }
-
-// create a function 'determinWinner' and pass in parameters
-// 'userChoice' and 'computerChoice'
-function determineWinner(userChoice, computerChoice){
+// create a function `determineWinner` and pass in parameters
+// `userChoice` and `computerChoice`
+function determineWinner(userChoice, computerChoice) {
     // if there is a tie
     if (userChoice === computerChoice) {
         return 'It is a tie';
     }
-    // if user selects 'human'
+    // if user selects `'human'`
     if (userChoice === 'human') {
-        // if computer selects 'bear'
-    } else {
-        return 'You have disarmed a gun';
+        // if computer selects `'bear'`
+        if (computerChoice === 'bear') {
+            return 'You have been mauled by a bear';
+        } else {
+            return 'You have disarmed a gun';
+        }
     }
-} 
-// if user selects 'bear'
-if (userChoice === 'bear') {
-    // if computer selects 'gun'
-    if (computerChoice === 'gun') {
-        return 'You have been shot by a gun';
-    } else {
-        return 'You mauled a human';
+    // if user selects `'bear'`
+    if (userChoice === 'bear') {
+        // if computer selects `'gun'`
+        if (computerChoice === 'gun') {
+            return 'You have been shot by a gun';
+        } else {
+            return 'You have mauled a human';
+        }
+    }
+    // if user selects `'gun'`
+    if (userChoice === 'gun') {
+        // if computer selectsion `'human'`
+        if (computerChoice === 'human') {
+            return 'Your gun has been disarmed';
+        } else {
+            return 'You have shot a bear';
+        }
     }
 }
 
-// if user selects a 'gun'
-if (userChoice === 'gun') {
- 
-}
