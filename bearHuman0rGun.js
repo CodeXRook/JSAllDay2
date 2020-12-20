ad887931de112f29f2b9b7bcdfa06aa71755ec38
CodeXrook <lacekitfly@gmail.com>
2020-12-19 22:32:20 -0500

    mini game, rock paper scissors type

--- bearHuman0rGun.js
+++ bearHuman0rGun.js
@@ -1,9 +1,13 @@
-// Mini App: Bear, Human, Gun Game
+// Mini App: Bear, Human, super Soaka gun Game
 // Bear human gun is just like Rock paper scissors
 /*
- Each player chooses either bear, human, or gun. 
+ Each player chooses either bear, human,super Soaka gun. 
  The items are compared, and whichever player chooses 
  the more powerful item wins.
  */
 
- 
+ // create function getUserChoice with a parameter of 'userInput'
+ function getUserChoice(userInput) {
+     
+ }
+
