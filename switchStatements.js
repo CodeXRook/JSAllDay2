d239bbdfb8e9f44e7076e00dae7bf5df8667a03f
CodeXrook <lacekitfly@gmail.com>
2021-02-03 00:36:09 -0500

    alert, coming for everything

--- switchStatements.js
+++ switchStatements.js
@@ -23,3 +23,59 @@ switch(gradeRemark){
 }
 
 // Case B GOOD SHIT
+
+// review switch statement
+// expression, case run code for match
+// break
+// default
+
+/* 
+ syntax
+
+   switch(expression) {
+    case a:
+     code block;
+     break;
+    case b:
+     code block;
+     break;
+    case c: 
+     code block;
+     break;
+     default:
+     code block;
+   }
+
+*/
+
+// Challenge 
+// Create switch with evaluation balue between 1-6
+// Have code block for each possible match to run
+// End with default code block
+// Expression (1-6) can be hard coded by you or you can create method for random number
+
+var dice = 6;
+switch(dice) {
+    case 1:
+    alert ('Terrible roll');
+    break;
+    case 2:
+    alert ('you suck, next');
+    break;
+    case 3:
+    alert ('damn dawg, nope');
+    break;
+    case 4 :
+    alert ('faaak');
+    break;
+    case 5: 
+    alert ('close but no');
+    break;
+    case 6:
+    alert ('Yes, finally you got 6');
+    break;
+    default:
+    alert ('My g did you roll');
+    break;
+}
+
