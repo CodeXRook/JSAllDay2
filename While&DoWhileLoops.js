3480877e4f2231489332c91e701798c9fdea29ed
CodeXrook <lacekitfly@gmail.com>
2021-02-07 05:55:05 -0500

    looper

--- While&DoWhileLoops.js
+++ While&DoWhileLoops.js
@@ -22,7 +22,6 @@ do{
     console.log(value);
     value++;
 } while (value < 13);
-
 /*
 5
 6
@@ -41,7 +40,6 @@ do{
 for(var i = 0; i <10; i++) {
     console.log('My favorite number is ' + i + '!');
 }
-
 /*
 My favorite number is 0!
 My favorite number is 1!
@@ -62,10 +60,58 @@ var arrayLength = zooNames.length; // 3
 for(i = 0; i < arrayLength; i++) {
     console.log('My favorite zoo animals is the ' + zooNames[i] + '!');
 }
-
 /*
 My favorite zoo animals is the lion!
 My favorite zoo animals is the tiger!
 My favorite zoo animals is the bear!
 */
 
+
+// REVIEW LOOPS PART2
+
+// Challenge
+// run a while loop 5x's
+// counter, while keyword, (expression), {code block}, update value
+
+var i = 0;
+while (i < 5) {
+    console.log('This counter has run ' + (i + 1) + ' times.');
+    i++;
+}
+/*
+This counter has run 1 times.
+This counter has run 2 times.
+This counter has run 3 times.
+This counter has run 4 times.
+This counter has run 5 times.
+*/
+
+
+// Challenge do/while
+// create one example theat run once with a false expression
+
+var counter = 5;
+do {
+    console.log('Loop ran even with false expression.');
+    counter++;
+} while (counter < 5);
+// Loop ran even with false expression.
+
+// For loop challenge
+// create and array number values
+// loop through that array with a for loop
+// run each element of the array through an if, else, else if clause with at least two else if options
+
+var battingAverage = [333, 327, 267, 294, 303, 310, 255, 242, 217];
+for (i = 0; i < battingAverage.length; i++) {
+  if( battingAverage[i] > 300) {
+      console.log('Here is a million dollar bonus');
+  } else if (battingAverage[i] > 280){
+      console.log('Here is a half million dollar bonus');
+  } else if (battingAverage[i] > 250) {
+      console.log('Try harder');
+  } else {
+      console.log("You're cut");
+  }
+}
+battingAverage(500);
