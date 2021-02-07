e09fe5ff584631251b43bedfa222be68d3f2ece2
CodeXrook <lacekitfly@gmail.com>
2021-02-07 04:50:51 -0500

    do it for a while

--- While&DoWhileLoops.js
+++ While&DoWhileLoops.js
@@ -5,3 +5,20 @@ while(score < 5) {
     console.log(score);
     score++;
 }
+// 0    
+// 1
+// 2
+// 3
+// 4
+// 5 is not less than 5, that's false.
+
+
+
+// Do/while
+var value = 5;
+do{
+    console.log(value);
+    value++;
+} while (value < 3);
+
+
