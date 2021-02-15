79a620582bf47e1be486b52445e32706469bda3f
CodeXrook <lacekitfly@gmail.com>
2021-02-14 22:46:25 -0500

    Func Pro

---
+++ Functional_Programming.js
@@ -0,0 +1,10 @@
+/* 
+JavaScript can handle higher-order functions.
+A higher-order function is a finction thart can 
+take another function as an argument, or that 
+returns a function as a result.
+*/
+
+
+
+
