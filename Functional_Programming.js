0a0642c4428c18a6155f5c57aee6a10420f1f073
CodeXrook <lacekitfly@gmail.com>
2021-02-21 11:48:24 -0500

    Def had imposter syndrome, but fuck dat I was effen born for this

--- Functional_Programming.js
+++ Functional_Programming.js
@@ -1,6 +1,6 @@
 /* 
 JavaScript can handle higher-order functions.
-A higher-order function is a finction thart can 
+A higher-order function is a function that can 
 take another function as an argument, or that 
 returns a function as a result.
 */
@@ -113,3 +113,61 @@ var forecast = [
 var sunnyDays = forecast.filter(function(weather) {
     return weather.sun;
 })
+
+
+// PURE FUNCTION
+// A function is pure if, given the same input,
+// it will always produce the same output.
+
+/* 
+Math.random() produces a new random number
+between 0 and 1 everytime you run it. This is an example of a 
+function that given the same input
+does not return the same output.
+ */
+
+ /* 
+ map() Creates a new array with the results
+ of calling a provided function on 
+ every element of an array.
+*/
+
+var forecast = [
+    {day: 'Monday', sun: true, humidity: 10},
+    {day: 'Tuesday', sun: false, humidity: 100},
+    {day: 'Wednesday', sun: true, humidity: 100},
+    {day: 'Thursday', sun: true, humidity: 25},
+    {day: 'Friday', sun: false, humidity: 100},
+    {day: 'Saturday', sun: true, humidity: 15},
+    {day: 'Sunday', sun: false, humidity: 100}
+];
+
+var humid = [];
+//Statement1 Counter //Statement2 Test condi // Statement3 Update once gone through
+for (var i = 0;   i < forecast.length;     i++) {
+    humid.push(forecast[i].humidity);
+}
+
+var humid = forecast.map(function(wetness) {
+    return wetness.humidity;
+})
+humid; 
+
+
+//////
+
+// review 
+// map
+// Map is a higher order function - can receive another function as an argument
+// does not delete but transforms each element in a new array
+
+// practice
+
+var numbers = [2, 4, 6, 8, 10, 12];
+
+var double = numbers.map(function(mult) {
+    return mult * 2;
+}).map(function(mult){
+    return mult + 1;
+})
+// double;[5, 9, 13, 17, 21, 25]
